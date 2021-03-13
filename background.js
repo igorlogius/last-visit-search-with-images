0d90045e9fe724af1a66d2a0ce806f11b8f2c00c
igorlogius <67047467+igorlogius@users.noreply.github.com>
2021-03-13 14:33:48 +0100

    add tab image to storage

---
+++ background.js
@@ -0,0 +1,87 @@
+
+
+async function saveToStorage(details) {
+
+	console.log('saveToStorage');
+
+	let tabInfo;
+	try {
+		tabInfo = await browser.tabs.get(details.tabId);
+	}catch(error){
+		console.error(error);
+		return;
+	}
+	console.log('>> tab with tabId still exists');
+
+	if( tabInfo.url !== details.url) {
+		console.log('>>[STOP] tab url changed from ' , details.url, " to ", tabInfo.url);
+		return;
+	}
+	console.log('>> tab url has not changed');
+
+	//  CaptureTab
+	
+	let imgUri;
+	try {
+		const options = {
+			"format": "jpeg"
+			,"quality": 25
+			//,"rect" : { "x": 0, "y": 0, "width": 100, "height": 100 }
+			//,"scale": 1
+		};
+		imgUri = await browser.tabs.captureTab(details.tabId,options);
+	}catch(error) {
+		console.error(error);
+		return;
+	}
+	//console.log(imgUri);
+		
+	let visualHistoryItems = {"visualHistoryItems": {}};
+	try {
+		visualHistoryItems = await browser.storage.local.get("visualHistoryItems");
+		//console.log('visualHistoryItems 1', visualHistoryItems);
+	}catch(error){
+		console.error(error);
+	}
+
+	const item = {
+		ts: details.timeStamp,
+		img: imgUri,
+		url: details.url
+	}
+	visualHistoryItems = visualHistoryItems['visualHistoryItems'];
+	visualHistoryItems[details.url] = item;
+
+	// reset / empty storage
+	//visualHistoryItems = {};
+
+	try {
+		await browser.storage.local.set({visualHistoryItems});
+		visualHistoryItems = await browser.storage.local.get("visualHistoryItems");
+		console.log('visualHistoryItems 2', visualHistoryItems);
+	}catch(error){
+		console.error(error);
+		return; 
+	}
+
+}
+
+function onCompleted(details) {
+
+	// Filter out any sub-frame related navigation event
+	if (details.frameId !== 0) {
+		return;
+	}
+	console.log(`>> onCompleted: ${details.url}`);
+
+	// create a timeout function which takes the sceenshot
+	//
+
+
+	setTimeout(function() {
+		console.log('setTimeout');
+		saveToStorage(details);
+	}, 1000);
+}
+
+browser.webNavigation.onCompleted.addListener(onCompleted);
