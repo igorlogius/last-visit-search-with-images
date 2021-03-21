9a0824de29e3d0858fe30830e965202c85f7632e
igorlogius <67047467+igorlogius@users.noreply.github.com>
2021-03-21 22:06:27 +0100

    feat: add graphical date filter

--- visualHistory.js
+++ visualHistory.js
@@ -1,7 +1,44 @@
 $(document).ready(async function() {
 
 
+		var dateFormat = "yy-mm-dd";
+	var table; 
+
+	$.fn.dataTable.ext.search.push(
+    function( settings, data, dataIndex ) {
+	var from_str = $('#from').val();
+	var to_str = $('#to').val();
+	var min = NaN;
+	var max = NaN;
+	    var date;
+
+	    console.log(from_str, " ", to_str, data[0]);
+
+	if(from_str !== "" ) {
+		date = $.datepicker.parseDate( dateFormat, from_str);
+        	min = parseInt( date.getTime(), 10 );
+	}
+	if(to_str !== "" ) {
+	    	date = $.datepicker.parseDate( dateFormat, to_str);
+        	max = date.getTime() + 24*60*60*1000;
+	}
+        var age = parseFloat( data[0] ) || 0; // use data for the age column
+
+	    console.log('min: ' + min, " max: ", max, " value: ", data[0]);
+
+        if ( ( isNaN( min ) && isNaN( max ) ) ||
+             ( isNaN( min ) && age <= max ) ||
+             ( min <= age   && isNaN( max ) ) ||
+             ( min <= age   && age <= max ) )
+        {
+            return true;
+        }
+        return false;
+    }
+);
 	async function loadTable(){
+
+
 
 		let visualHistoryItems = {"visualHistoryItems": {}};
 		try {
@@ -22,7 +59,7 @@ $(document).ready(async function() {
 			dtdata.push(value);
 		});
 
-		var table = $('#myTable').DataTable( {
+		table = $('#myTable').DataTable( {
 			"columnDefs": [
 			    { orderable: false , targets: 2}
   			],
@@ -36,17 +73,17 @@ $(document).ready(async function() {
 				{ "data": "ts" 
 					, "render": function(data, type, row, meta) {
 						const d = new Date(data);
-						let t = d.getFullYear();
-						let str = t + "-";
-						t = d.getMonth();
+						str = d.getFullYear() + "-";
+						t = "";
+						t = (d.getMonth()+1)
 						str = str + ((t<10)? "0":"") + t + "-";
-						t = d.getDay();
+						t = (d.getDate())
 						str = str + ((t<10)? "0":"") + t + " ";
-						
-						t = d.getHours();
+
+						t = (d.getHours())
 						str = str + ((t<10)? "0":"") + t + ":";
-						t = d.getMinutes();
-						str = str + ((t<10)? "0":"") + t + "";
+						t = (d.getMinutes())
+						str = str + ((t<10)? "0":"") + t;
 
 						return type === 'display' ? str : data;
 					}
@@ -115,6 +152,46 @@ $(document).ready(async function() {
 		} );
 	}
 	loadTable();
+
+
+			var from = $( "#from" )
+			.datepicker({
+				dateFormat: dateFormat,
+				//defaultDate: "+1w",
+				changeMonth: true,
+				changeYear: true,
+				numberOfMonths: 1
+			})
+			.on( "change", function() {
+				to.datepicker( "option", "minDate", getDate( this ) );
+				table.draw();
+			}),
+			to = $( "#to" ).datepicker({
+				dateFormat: dateFormat,
+				//defaultDate: "+1w",
+				changeMonth: true,
+				changeYear: true,
+				numberOfMonths: 1
+			})
+			.on( "change", function() {
+				from.datepicker( "option", "maxDate", getDate( this ) );
+				table.draw();
+			});
+
+		function getDate( element ) {
+			var date;
+			try {
+				console.log(element.value);
+				date = $.datepicker.parseDate( dateFormat, element.value );
+				console.log(date.getTime()/1000);
+			 	
+			} catch( error ) {
+				date = null;
+				console.error(error);
+			}
+
+			return date;
+		}
 
 } );
 
