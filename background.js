24e84518324fc8b0ae6a81eb2450e046225bdb0d
igorlogius <67047467+igorlogius@users.noreply.github.com>
2021-04-05 21:03:51 +0200

    feat(#3) add option to set delay time for item creation

--- background.js
+++ background.js
@@ -1,20 +1,24 @@
 
-const extId="visualHistory";
-
 const temporary = browser.runtime.id.endsWith('@temporary-addon'); // debugging?
+const manifest = browser.runtime.getManifest();
+const extname = manifest.name;
 
-const log = (level, msg) => {
-	level = level.trim().toLowerCase();
-	if (['error','warn'].includes(level)
-		|| ( temporary && ['debug','info','log'].includes(level))
-	) {
-		console[level](extId + '::' + level.toUpperCase() + '::' + msg);
-		return;
+function log() {
+	if(arguments.length < 2){
+		throw 'invalid number of arguments';
 	}
-};
+	const level = arguments[0].trim().toLowerCase();
+	let msg = '';
+	for (let i=1; i < arguments.length; i++) {
+		msg = msg + arguments[i];
+	}
+	if (['error','warn'].includes(level) || ( temporary && ['debug','info','log'].includes(level))) {
+		console[level]('[' + extname + '] [' + level.toUpperCase() + '] ' + msg);
+	}
+}
 
 
-const saveToStorage = async (details) => {
+async function saveToStorage (details) {
 	// 
 	const tabInfo = await (async () => {
 		try {
@@ -25,14 +29,14 @@ const saveToStorage = async (details) => {
 		}
 	})();
 	if(tabInfo === null){return;}
-	log('DEBUG', "[CONTINUE] tabId " + details.tabId + " exists");
+	//log('DEBUG', "[CONTINUE] tabId " + details.tabId + " exists");
 
 	//
 	if( tabInfo.url !== details.url) {
 		log('DEBUG', "[STOPPING] tabID "+ details.tabId +" with url " + details.url + "does not exist");
 		return;
 	}
-	log('DEBUG', "[CONTINUE] tabId "+ details.tabId +" with url " + details.url + " exist");
+	//log('DEBUG', "[CONTINUE] tabId "+ details.tabId +" with url " + details.url + " exist");
 
 	// 
 	const imgUri = await (async () => {
@@ -45,7 +49,7 @@ const saveToStorage = async (details) => {
 		}
 	})();
 	if(imgUri === null){return;}
-	log('DEBUG', "[CONTINUE] tabId "+ details.tabId +" captured");
+	//log('DEBUG', "[CONTINUE] tabId "+ details.tabId +" captured");
 		
 	//
 	try {
@@ -55,35 +59,53 @@ const saveToStorage = async (details) => {
 		url: details.url
 	});
 	//
-		log('DEBUG', "[CONTINUE] tabId " + details.tabId + " wrote item to storage");
+		//log('DEBUG', "[CONTINUE] tabId " + details.tabId + " wrote item to storage");
 	}catch(error){
 		log('ERROR', "[STOPPING] tabId " + details.tabId + " failed to write item to storage");
 	}
 };
 
-const onCompleted = (details) => {
+async function onCompleted (details) {
 
 	if (details.frameId !== 0) { 
-		log('DEBUG', "[STOPPING] tabId " + details.tabId + " with url " + details.url + " is not a main frame"); 
+		//log('DEBUG', "[STOPPING] tabId " + details.tabId + " with url " + details.url + " is not a main frame"); 
 		return; 
 	}
-	log('DEBUG', "[CONTINUE] tabId " + details.tabId + " with url " + details.url + " is a main frame"); 
+	//log('DEBUG', "[CONTINUE] tabId " + details.tabId + " with url " + details.url + " is a main frame"); 
+	//if ( !/^https?:\/\//.test(details.url) ){ 
+	//	log('DEBUG', "[STOPPING] tabId " + details.tabId + " with url " + details.url + " has an invalid protocol"); 
+	//	return; 
+	//}
+	//log('DEBUG', "[CONTINUE] tabId " + details.tabId + " with url " + details.url + " has an valid protocol"); 
 
-	if ( !/^https?:\/\//.test(details.url) ){ 
-		log('DEBUG', "[STOPPING] tabId " + details.tabId + " with url " + details.url + " has an invalid protocol"); 
-		return; 
-	}
-	log('DEBUG', "[CONTINUE] tabId " + details.tabId + " with url " + details.url + " has an valid protocol"); 
+	const delaytime = (await (async () => {
+		try {
+			let tmp = await browser.storage.local.get('delaytime');
+			if (typeof tmp['delaytime'] !== 'undefined'){
+				tmp = parseInt(tmp['delaytime']);
+			}
+			//console.log('tmp ', typeof tmp);
+			
+			if(typeof tmp === 'number') {
+				if(tmp > 999){
+					return tmp;
+				}
+			}
+		}catch(e){
+			console.error(e);
+		}
+		return 5000;
+		
+	})());
 
-	setTimeout( () => {
-		saveToStorage(details);
-	}, 5000);
-};
+	//log('debug', '[INFO] delaytime := ' + delaytime);
+	setTimeout( () => { saveToStorage(details); }, delaytime );
+}
 
-const onBrowserActionClicked = (tab) => { 
-
+function onBrowserActionClicked(tab) { 
 	browser.tabs.create({url: "visualHistory.html"});
-};
+}
 
-browser.webNavigation.onCompleted.addListener(onCompleted); 
+browser.webNavigation.onCompleted.addListener(onCompleted, { url: [ {schemes: ["http","https"]}]} ); 
 browser.browserAction.onClicked.addListener(onBrowserActionClicked);
+
